858732a99fc5d203a042214a7f9a01810e69163b
Elysia <114798157+KevinKyun@users.noreply.github.com>
2024-06-05 12:10:23 +0700

    Add files via upload

---
+++ CRUD/form_edit.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Tambah Mahasiswa</title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+    </head>
+    <body>
+        <div class="container">
+            <h1 class="mt-4 mb-4">Tambah Mahasiswa</h1>
+</html>
